ba50db1b6dae8b4f971f6822523792646c13605d
SHAHZADASOHRAB420 <71369025+SHAHZADASOHRAB420@users.noreply.github.com>
2020-09-28 10:24:14 +0430

    Add files via upload

---
+++ khan_style.py
@@ -0,0 +1,25 @@
+command_not_found_handle() {
+	/data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+
+}
+echo -e "\e[1;92m "
+echo "   ⊢□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■■□■□■□■□■□■□■□■□■⊣"
+echo -e "\e[1;92m"
+echo " ╭━ ♥━ ♥━ ♥━ ♥━ ♥━ ♥━ ♥━"
+echo " ╰╮┏━┳┳┳┓  ┏┳┳┳┳┳┓"
+echo "┏┳┳┳┳┳┓"
+echo" ┏┻╋━╋┻┻┫  ┣┻┻┻┻┻┫ "
+echo" ┣┻┻┻┻┻┫
+echo" ┗ⓞ━━━ⓞ┻━┻ⓞ━━ⓞ┻━┻ⓞ━━ⓞ╯"
+echo" \033[1;92m ♤♡♤♡♤♡ EDIT BY AMPARATOR"
+echo -e "\e[1;92m"
+echo "   ⊢□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■■□■□■□■□■□■□■□■□■□■⊣"
+echo ""
+echo "" 
+echo -e "\e[1;91m                [\e[1;96m*\e[1;91m] \e[1;97mWhatsapp    \e[1;91m =  \e[1;97m+937886178"
+echo ""
+echo -e "\e[1;91m                [\e[1;96m*\e[1;91m] \e[1;97mTelegram  \e[1;91m =  \e[1;97mt.me/ShAhZaDa_SoHrAb_420"
+echo ""
+echo ''
+
+PS1='\[\e[31m\]┌━[\[\e[37m\]\T\[\e[31m\]]━━━━━━\e[1;98m[Amp4artor]\e[0;31m━━━━━━━[\#]\n|\n\e[0;31m└━━[\[\e[31m\]\e[0;35m\W\[\e[31m\]]━━━━➤ \e[1;92m'
